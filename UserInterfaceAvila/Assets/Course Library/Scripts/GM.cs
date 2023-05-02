75f67af93508e63f41a5996b74f93cc8a134d0f6
MatthewAvila <709938@student.dusd.net>
2023-05-02 11:36:50 -0700

    POINT SYSTEM
    targets are now worth points

--- UserInterfaceAvila/Assets/Course Library/Scripts/GM.cs
+++ UserInterfaceAvila/Assets/Course Library/Scripts/GM.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEditor.Build.Player;
 
 public class GM : MonoBehaviour
 {
     public List<GameObject> targets;
+    public TextMeshProUGUI scoreText;
+    private int score;
     private float spawnRate = 0.75f;
+
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
     }
 
     // Update is called once per frame
@@ -25,6 +32,13 @@ public class GM : MonoBehaviour
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
+
         }
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
     }
 }
--- UserInterfaceAvila/Assets/Course Library/Scripts/Target.cs
+++ UserInterfaceAvila/Assets/Course Library/Scripts/Target.cs
@@ -1,19 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Build.Content;
 using UnityEngine;
 
 public class Target : MonoBehaviour
 {
     private Rigidbody targetRb;
+    private GM gm; 
     private float minSpeed = 12;
     private float maxSpeed = 16;
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPos = -2;
 
+    public int pointValue;
+
     // Start is called before the first frame update
     void Start()
     {
+        gm = GameObject.Find("GM").GetComponent<GM>();
         targetRb = GetComponent<Rigidbody>();
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
         targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
@@ -23,6 +28,7 @@ public class Target : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
+        gm.UpdateScore(pointValue);
     }
 
     private void OnTriggerEnter(Collider other)
